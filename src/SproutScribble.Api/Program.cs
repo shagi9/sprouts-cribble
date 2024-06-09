804055617cd2e5657288ab60a1b0728767b33b04
sdt-IlliaShahoferov <ishahoferov@stratadecision.com>
2024-06-09 21:28:26 +0300

    Remove ignored files from repository

--- src/SproutScribble.Api/Program.cs
+++ src/SproutScribble.Api/Program.cs
@@ -1,6 +1,28 @@
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = string.Empty;
+    });
+}
+
+app.MapGet("/weathercasr", () => "Hello World!")
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
 
 app.Run();
